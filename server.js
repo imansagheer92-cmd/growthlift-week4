d52483616ec19ee7f49ac737ef094e7357e234d0
Iman Sagheer <imansagheer92@gmail.com>
2026-09-16 17:00:36 +0500

    Week 4 Day 3: REST API CRUD with status codes

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+let tasks = [
+    {
+        id: 1,
+        title: "Learn Express",
+        done: false
+    },
+    {
+        id: 2,
+        title: "Build REST API",
+        done: false
+    }
+];
+
 const express = require("express");
 
 const app = express();
@@ -38,4 +51,59 @@ app.get("/api/search", (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+});
+
+app.get("/api/tasks", (req, res) => {
+    res.json(tasks);
+});
+
+app.get("/api/tasks/:id", (req, res) => {
+    const task = tasks.find(
+        t => t.id === parseInt(req.params.id)
+    );
+
+    if (!task) {
+        return res.status(404).json({
+            message: "Task not found"
+        });
+    }
+
+    res.json(task);
+});
+
+app.put("/api/tasks/:id", (req, res) => {
+    const task = tasks.find(
+        t => t.id === parseInt(req.params.id)
+    );
+
+    if (!task) {
+        return res.status(404).json({
+            message: "Not found"
+        });
+    }
+
+    task.title = req.body.title;
+    task.done = req.body.done;
+
+    res.json(task);
+});
+
+app.post("/api/tasks", (req, res) => {
+    const newTask = {
+        id: tasks.length + 1,
+        title: req.body.title,
+        done: false
+    };
+
+    tasks.push(newTask);
+
+    res.status(201).json(newTask);
+});
+
+app.delete("/api/tasks/:id", (req, res) => {
+    tasks = tasks.filter(
+        t => t.id !== parseInt(req.params.id)
+    );
+
+    res.status(204).send();
 });
